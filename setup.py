9915e1f2d3fd8448c6061898bf24e4f029c13e1c
Thiago Berlitz Rondon <thiago.rondon@gmail.com>
2011-06-24 23:34:08 -0300

    oops, setup.py.
    Signed-off-by: Thiago Berlitz Rondon <thiago.rondon@gmail.com>

---
+++ setup.py
@@ -0,0 +1,14 @@
+
+from setuptools import setup, find_packages
+
+setup(name="nsms_web_api",
+    version='0.0.1',
+    description='API para envio de SMS em Python.',
+    author='Thiago Rondon',
+    author_email='thiago@nsms.com.br',
+    url='http://www.nsms.com.br',
+    py_modules=['nsms_web_api'],
+    test_suite="test",
+)
+
+
